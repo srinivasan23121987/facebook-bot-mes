9462e299bb1002f6eb5a91f85d1046990dd04f1b
Srinivasan Velu <40140@hexaware.com>
2018-06-21 09:14:39 +0530

    new commit

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var MessengerPlatform = require('facebook-bot-messenger');
 var app = require('express')();
 var server = require('http').Server(app);
 var bot = MessengerPlatform.create({
-  pageID: '<your page id>',
-  appID: '118602068794174',
-  appSecret: '<your app secret>',
-  validationToken: '<your validation token>',
-  pageToken: '<your page token>'
+    pageID: '<your page id>',
+    appID: '118602068794174',
+    appSecret: '<your app secret>',
+    validationToken: '<your validation token>',
+    pageToken: '<your page token>'
 }, server);
 app.use(bot.webhook('/webhook'));
-bot.on(MessengerPlatform.Events.MESSAGE, function(userId, message) {
-
+bot.on(MessengerPlatform.Events.MESSAGE, function (userId, message) {
+console.log(userId + "=>" +message);
 });
 server.listen(5253 || process.env.PORT);
