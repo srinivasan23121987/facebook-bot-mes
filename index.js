9ce2d2243426ba6d55180558e90ac4ed296d24c9
Srinivasan Velu <40140@hexaware.com>
2018-06-21 10:57:07 +0530

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var MessengerPlatform = require('facebook-bot-messenger');
 
 var app = require('express')();
 var server = require('http').Server(app);
+let bodyParser = require('body-parser');
 var bot = MessengerPlatform.create({
     pageID: '1823796064585969',
     appID: '938799082965836',
@@ -10,11 +11,11 @@ var bot = MessengerPlatform.create({
     pageToken: 'EAANV1THbC0wBANGJFgwHNoKZBnhW3u8Q52LI7OvUa9VCNYuAHypG4xxPAMr3FtBMBhT6AcoG86O62NZAjBIVGX0dQ3oOTtbNZAiZCtdynMd1hEGR6VISk9e8AKScaUPxjKlRBs6qRTTSUqbuv2BpWifh5VohyXYRggOSXJSCGQZDZD'
 }, server);
 app.use(bot.webhook('/webhook'));
+app.use(bodyParser.json());
 bot.on(MessengerPlatform.Events.MESSAGE, function (userId, message) {
-console.log(userId + "=>" +message);
+    console.log(userId + "=>" + message);
 });
-app.post("/slackwebhook",function(req,res){
-console.log(res);
-res.send("Hi");
+app.post("/slackwebhook", function (req, res) {
+    res.send(req.body.challenge);
 })
 server.listen(3000 || process.env.PORT);
